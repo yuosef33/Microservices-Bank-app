3e89b75314b76313f5d540246f839a3c84873549
yuosef <jamalyuosef0@gmail.com>
2025-09-20 17:12:56 +0300

    adding configserver, rappitmq and github webhook with hookdeck

--- accounts/src/main/java/com/yuosef/accounts/Dtos/AccountsContactInfoData.java
+++ accounts/src/main/java/com/yuosef/accounts/Dtos/AccountsContactInfoData.java
@@ -1,15 +1,20 @@
 package com.yuosef.accounts.Dtos;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "accounts")
-public record AccountsContactInfoData(
-        String message,
-        Map<String, String> contactDetails,
-        List<String> onCallSupport
-) {
+@Getter
+@Setter
+public class AccountsContactInfoData{
+
+        String message;
+        Map<String, String> contactDetails;
+        List<String> onCallSupport;
+
 
 }
--- cards/src/main/java/com/eazybytes/cards/dto/AccountContactInfoData.java
+++ cards/src/main/java/com/eazybytes/cards/dto/AccountContactInfoData.java
@@ -1,14 +1,19 @@
 package com.eazybytes.cards.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "accounts")
-public record AccountContactInfoData (
-        String message,
-        Map<String, String> contactDetails,
-        List<String> onCallSupport
-){
+@Getter
+@Setter
+public class AccountContactInfoData {
+
+    String message;
+    Map<String, String> contactDetails;
+    List<String> onCallSupport;
+
 }
